74cb3a7ce208cb25366eb25c1aca04e6b217a690
jamal23041989 <jamalkurbanov23041989@gmail.com>
2026-01-15 23:20:00 +0300

    create custom errors

--- internal/core/errors/errors.go
+++ internal/core/errors/errors.go
@@ -1,8 +1,27 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
+
+//type ClientError interface {
+//	Error() string
+//	IsClientError() bool
+//}
 
 var (
-	ErrProductNotFound = errors.New("product not found")
-	ErrInvalidInput    = errors.New("invalid input data") // Наша новая метка 🏷️
+	ErrProductNotFound     = errors.New("product not found")
+	ErrInvalidInput        = errors.New("invalid input data")
+	ErrMethodNotAllowed    = errors.New("method not allowed")
+	ErrInternalServerError = errors.New("internal server error")
 )
+
+type ValidationError struct {
+	Field string
+	Value string
+}
+
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("invalid value %s in field %s", e.Value, e.Field)
+}
